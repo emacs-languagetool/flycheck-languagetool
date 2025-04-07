e44bd8bf7ec481bf8911435544becc4bef74e9e8
JenChieh <jcs090218@gmail.com>
2025-04-06 17:21:56 -0700

    feat: flycheck no longer support Emacs 25 and 26

--- flycheck-languagetool.el
+++ flycheck-languagetool.el
@@ -7,7 +7,7 @@
 ;;         Peter Oliver <git@mavit.org.uk>
 ;; URL: https://github.com/emacs-languagetool/flycheck-languagetool
 ;; Version: 0.3.0
-;; Package-Requires: ((emacs "25.1") (flycheck "0.14"))
+;; Package-Requires: ((emacs "27.1") (flycheck "0.14"))
 ;; Keywords: convenience grammar check
 
 ;; This file is NOT part of GNU Emacs.
